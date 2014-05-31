e13a4cafda49f63e6c687af517795af6021a76c6
Mark Durrant <mark@pebblecode.com>
2014-05-31 12:55:24 +0100

    add all the things!

---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+$( document ).ready( function() {
+
+  // COPY PASTED FROM STACK OVERLFOW - http://stackoverflow.com/questions/10238084/ios-safari-how-to-disable-overscroll-but-allow-scrollable-divs-to-scroll-norma
+
+  // stops page scrolling
+
+  // Uses document because document will be topmost level in bubbling
+  $(document).on('touchmove',function(e){
+    e.preventDefault();
+  });
+
+  var scrolling = false;
+
+  // Uses body because jquery on events are called off of the element they are
+  // added to, so bubbling would not work if we used document instead.
+  $('body').on('touchstart','.slider',function(e) {
+
+      // Only execute the below code once at a time
+      if (!scrolling) {
+          scrolling = true;
+          if (e.currentTarget.scrollTop === 0) {
+            e.currentTarget.scrollTop = 1;
+          } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+            e.currentTarget.scrollTop -= 1;
+          }
+          scrolling = false;
+      }
+  });
+
+  // Prevents preventDefault from being called on document if it sees a slider div
+  $('body').on('touchmove','.slider',function(e) {
+    e.stopPropagation();
+  });
+
+  // end opy paste
+
+  $( '.intro' ).click( function() {
+    $( this ).removeClass( 'active' )
+      .addClass( 'done' )
+      .next().addClass( 'active' );
+  });
+
+  $( '.smiley' ).click( function() {
+    $( this ).parent().removeClass( 'active' )
+      .addClass( 'done' )
+      .next().addClass( 'active' );
+  });
+
+});
